1529b1ab59f8c6e97a8c104fa1c7e9e0a1905ca3
Elias Franco <eliasfranco2379@gmail.com>
2021-04-02 18:49:14 -0400

    Day 4 - conditionals

---
+++ 04-Day/main.js
@@ -0,0 +1,27 @@
+
+//      Exerscise level 1
+// let edad = Number(prompt('Introduzca su edad'));
+// if( edad >= 18 )
+//     console.log('tu eres mayor, puedes conducir');
+// else {
+//     console.log(`Te faltan ${18-edad} años para poder conducir`);
+// }
+
+// let miEdad = Number(prompt('Introduzca su edad'));
+// let tuEdad = Number(prompt('Introci tu edad'));
+// if(miEdad > tuEdad)
+//     console.log(`Soy tu mayor con ${miEdad-tuEdad} años`);
+// else
+//     console.log(`Tu eres mayor q mi con ${tuEdad-miEdad}`);
+    
+
+let a = 44, b = 44;
+if( a > b )
+    console.log(`${a} es mayor que ${b}`);
+else 
+    console.log(`${b} es mayor que ${a}`);
+    
+console.log('below is ternari operator');
+
+let imprimir = a > b? console.log(`${a} es mayor que ${b}`): console.log(`${b} es mayor que ${a}`);
+;
